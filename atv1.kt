54ae9a142a03976eb2bfb8f688e6b05bbcda2e8f
Sara Angela <147667567+Chaverinh0@users.noreply.github.com>
2025-03-06 12:03:38 -0300

    atv1

---
+++ atv1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //1 Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário.
+
+    // Criando a variável e atribuindo o valor "Fusca"
+    var nome_do_carro = "Fusca"
+
+    // Exibindo a variável ao usuário
+    println (nome_do_carro)
+
+}
