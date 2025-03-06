38a164f4720150a57228261e07e5bba1d45b98d4
sara.asilva43 <saraprado535@gmail.com>
2025-03-06 12:30:06 -0300

    atv2

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Pede ao usuário para digitar o nome
+    print("Digite seu nome: ")
+
+    // Lê o nome digitado pelo usuário
+    val nome = readLine()
+
+    // Exibe a mensagem personalizada
+    println("Olá, $nome!")
+}
